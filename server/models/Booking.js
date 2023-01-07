1c49378eca01836d4640f28418f392ba918484f8
Samuel <samuelyongw@gmail.com>
2023-01-07 19:28:52 +1100

    Added models, and schema, TODO add seed and test

---
+++ server/models/Booking.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema({
+  CreatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  // https://mongoosejs.com/docs/tutorials/dates.html Doc about type: Date
+  BookedDate: {
+    type: Date,
+    // YYYY-MM-DD Format
+    // the minimum the date can be in 1st Jan 2023.
+    min: '2023-01-01'
+  },
+  // BookedBy
+   User: {
+    type:Schema.Types.ObjectId,
+    ref: 'User'
+  },
+});
+
+bookingSchema.methods.getBookedDate = () => {
+  return bookingSchema.BookedDate
+};
+
+const Booking = mongoose.model('Order', bookingSchema);
+
+module.exports = Booking;
