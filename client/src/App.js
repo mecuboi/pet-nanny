220fc8b0169d7d2c077fd600ce5bb5b369be2b68
mecuboi <matthewd.jusuf@gmail.com>
2023-01-09 21:15:17 +1100

    basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Home from './pages/Home';
 // import Detail from './pages/Detail';
