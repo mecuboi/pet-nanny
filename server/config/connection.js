a19c08be0597b4ae920ace9aaddbf6e161287f02
Samuel <samuelyongw@gmail.com>
2023-01-10 21:38:06 +1100

    Moved package.json dependendies to the apprioriate locations

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
+// const autoIncrement = require('mongoose-auto-increment');
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pet-nanny', {
